7a20beae9a0bcd211c7c74bfdee0f3339cbc1015
e-hokge777 <e.hokage777@gmail.com>
2025-12-14 18:50:04 +0000

    reload model correction

--- app/_components/3d/phone-model.tsx
+++ app/_components/3d/phone-model.tsx
@@ -71,4 +71,4 @@ export const PhoneModel = forwardRef<THREE.Group, PhoneModelProps>(
   }
 );
 
-useGLTF.preload("/apple_iphone_based_mobile_phone.glb");
+useGLTF.preload("/models/apple_iphone_based_mobile_phone.glb");
